c646f574bc195adc5d69e98782248007e1888fd9
Yooooo <pangxie.wdy@gmail.com>
2022-11-04 11:41:41 +0800

    chore: remove unnessary script

--- build.js
+++
@@ -1,7 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const pkg = require('../package.json');
-
-(function () {
-  const { version } = pkg;
-  console.log(`wooyoo.homelab-${version}`);
-}());
