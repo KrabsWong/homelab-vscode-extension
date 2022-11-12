17401e95a37f570a2bc048bade0793cd006a23fd
Yooooo <pangxie.wdy@gmail.com>
2022-11-12 23:01:48 +0800

    chore: 移除无用的注释

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,3 @@
-/** 由于 vscode-nls 强制要求必须要放到顶部最先初始化，所以当前文档的import顺序要做调整
- * https://www.npmjs.com/package/vscode-nls
- * This must be the first import in the main entry file
- * import * as nls from 'vscode-nls';
-*/
 import * as vscode from 'vscode';
 
 import { initialize as initializeHomelabLinks } from '@/provider/homelab-links';
